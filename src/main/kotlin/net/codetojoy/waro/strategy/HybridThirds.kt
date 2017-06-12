c52b711df543db80db1687e4e02cff15e744fc2a
Michael Easter <codetojoy@gmail.com>
2017-06-12 16:56:38 -0300

    new HybridThirds strategy

---
+++ src/main/kotlin/net/codetojoy/waro/strategy/HybridThirds.kt
@@ -0,0 +1,49 @@
+
+package net.codetojoy.waro.strategy
+
+class HybridThirds(val high:Strategy, val mid:Strategy, val low:Strategy) : Strategy {
+    enum class Range {
+        highRange,
+        midRange,
+        lowRange
+    }
+
+    override fun selectCard(prizeCard: Int, hand: List<Int>, maxCard: Int) = 
+        when(getRange(prizeCard, maxCard)) {
+            Range.highRange -> high.selectCard(prizeCard, hand, maxCard)
+            Range.midRange -> mid.selectCard(prizeCard, hand, maxCard)
+            else -> low.selectCard(prizeCard, hand, maxCard)
+        }
+
+    /*
+    override 
+    fun selectCard(prizeCard: Int, hand: List<Int>, maxCard: Int): Int {
+        var result = -1
+                
+        if (prizeCard > (maxCard * (2/3.0)) ) {
+            println("TRACER high ${prizeCard} ${maxCard}");
+            result = high.selectCard(prizeCard, hand, maxCard)
+        } else if (prizeCard > (maxCard/3.0) ) {
+            println("TRACER mid");
+            result = mid.selectCard(prizeCard, hand, maxCard)
+        } else {
+            println("TRACER low");
+            result = low.selectCard(prizeCard, hand, maxCard)            
+        }
+        
+        return result        
+    } 
+    */
+
+    internal fun getRange(prize: Int, max: Int): Range {
+        var result = Range.lowRange
+
+        if (prize > (max * (2/3.0)) ) {
+            result = Range.highRange
+        } else if (prize > (max/3.0) ) {
+            result = Range.midRange
+        }
+
+        return result
+    }
+}
---
+++ src/test/kotlin/net/codetojoy/waro/strategy/HybridThirdsCardTest.kt
@@ -0,0 +1,35 @@
+package net.codetojoy.waro.strategy
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class HybridThirdsTestSource {
+    val strategy = HybridThirds(MaxCard(), PopCard(), MinCard()) 
+    val hand = listOf(11, 1, 33, 44, 22)
+    val maxCard = 60
+
+    @Test fun testSelectCard_Low() {
+        val prizeCard = 10
+
+        // test
+        val result = strategy.selectCard(prizeCard, hand, maxCard)
+
+        assertEquals(1, result)
+    }
+    @Test fun testSelectCard_Mid() {
+        val prizeCard = 35
+
+        // test
+        val result = strategy.selectCard(prizeCard, hand, maxCard)
+
+        assertEquals(11, result)
+    }
+    @Test fun testSelectCard_High() {
+        val prizeCard = 50
+
+        // test
+        val result = strategy.selectCard(prizeCard, hand, maxCard)
+
+        assertEquals(44, result)
+    }
+}
